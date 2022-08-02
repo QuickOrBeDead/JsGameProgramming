4c4dd93de2393534ad5421d7e054529602f74c71
QuickOrBeDead <boraakgn@gmail.com>
2022-08-02 13:02:45 +0300

    add 003-parallax-background-with-vanilla-javascript

---
+++ src/003-parallax-background-with-vanilla-javascript/main.js
@@ -0,0 +1,66 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = canvas.width = 272;
+const CANVAS_HEIGHT = canvas.height = 160;
+
+class ParallaxImage {
+    constructor(image, x, y, width, height, speed, direction) {
+        this.image = image;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.direction = direction;
+    }
+
+    update() {
+        this.x += this.speed * this.direction;
+
+        if (this.x % this.width === 0) {
+            this.x = 0;
+        }
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+
+        if (this.speed) {
+            ctx.drawImage(this.image, 0, 0, Math.abs(this.x), this.height, this.x - (this.direction * this.width), this.y, Math.abs(this.x), this.height);
+        }
+    }
+}
+
+function createImage(path) {
+    const image = new Image();
+    image.src = path;
+    return image;    
+}
+
+const direction = -1;
+const images = [
+    new ParallaxImage(createImage("assets/bg.png"), 0, 0, 272, 160, 0, direction),
+    new ParallaxImage(createImage("assets/far-buildings.png"), 0, 18, 272, 142, 1, direction),
+    new ParallaxImage(createImage("assets/buildings.png"), 0, 10, 272, 150, 2, direction),
+    new ParallaxImage(createImage("assets/skill-foreground.png"), 0, 56, 272, 104, 4, direction)
+];
+
+const frameRate = 5;
+let frameCounter = 0;
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    images.forEach(i => i.draw(ctx));
+
+    if (frameCounter % frameRate === 0) {
+        images.forEach(i => i.update());
+    }
+
+    requestAnimationFrame(animate);
+
+    frameCounter++;
+}
+
+animate();
